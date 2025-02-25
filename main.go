ce4626e34c6226d0a367a765e2ac3dc2d5d798a5
Leonardo Pinheiro <leonardo.pinheiro@unilab.ind.br>
2025-02-25 16:32:45 -0300

    Fix

--- main.go
+++ main.go
@@ -2,28 +2,30 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
+	_ "encoding/csv"
 	"fmt"
 	"github.com/urfave/cli/v3"
 	"github.com/xuri/excelize/v2"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
 func createMap(statusFile string) map[string]string {
-	file, err := excelize.OpenFile(statusFile)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
+	rows := readFiles(statusFile)
 
 	pedidoList := map[string]string{}
 
-	rows, err := file.GetRows(file.GetSheetName(0))
-	if err != nil {
-		log.Fatalln(err)
-	}
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+	//rows, err := file.GetRows(file.GetSheetName(0))
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
 
 	for _, row := range rows {
 		if len(row) >= 4 && strings.Contains(strings.ToLower(row[3]), "cancelado") {
@@ -40,23 +42,74 @@ func createMap(statusFile string) map[string]string {
 	return pedidoList
 }
 
-func readPedidos(base string) [][]string {
-	file, err := excelize.OpenFile(base)
+func readFiles(base string) [][]string {
+	if filepath.Ext(base) == ".xlsx" {
+		file, err := excelize.OpenFile(base)
 
-	if err != nil {
-		log.Fatalln(err)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		rows, err := file.GetRows(file.GetSheetName(0))
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		return rows
 	}
 
-	rows, err := file.GetRows(file.GetSheetName(0))
-	if err != nil {
-		log.Fatalln(err)
+	if filepath.Ext(base) == ".csv" {
+		file, err := os.Open(base)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer func(file *os.File) {
+			err := file.Close()
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}(file)
+
+		reader := csv.NewReader(file)
+		reader.Comma = ';'
+
+		rows, err := reader.ReadAll()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		return rows
 	}
 
-	return rows
+	return [][]string{}
 }
 
 func createNewSheet() *excelize.File {
 	return excelize.NewFile()
+}
+
+func getCsvWriter(fileName string) *csv.Writer {
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//defer func(file *os.File) {
+	//	err := file.Close()
+	//	if err != nil {
+	//		log.Fatalln(err)
+	//	}
+	//}(file)
+
+	return csv.NewWriter(file)
+}
+
+func writeDataCsvFile(w *csv.Writer, row []string) {
+	err := w.Write(row)
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func writeCellValue(file *excelize.File, cell string, value string) {
@@ -66,12 +119,29 @@ func writeCellValue(file *excelize.File, cell string, value string) {
 	}
 }
 
-func generateFile(base string, status string) *excelize.File {
+func getTipo(base string, status string) string {
+	if filepath.Ext(base) == ".xlsx" || filepath.Ext(status) == ".xlsx" {
+		return "xlsx"
+	}
+
+	return "csv"
+}
+
+func generateFile(base string, status string, fileName string) (*excelize.File, *csv.Writer) {
 	pedidoList := createMap(status)
 
-	rows := readPedidos(base)
+	rows := readFiles(base)
 
-	newSheet := createNewSheet()
+	tipo := getTipo(base, status)
+	var newCsv *csv.Writer
+	var newSheet *excelize.File
+
+	if tipo == "csv" {
+		newCsv = getCsvWriter(fileName)
+		defer newCsv.Flush()
+	} else {
+		newSheet = createNewSheet()
+	}
 
 	letters := []string{
 		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
@@ -107,8 +177,18 @@ func generateFile(base string, status string) *excelize.File {
 		corte := "SEM CORTE"
 
 		if index == 0 {
+			var record []string
+
 			for idx, col := range columns {
-				writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[idx], 1), col)
+				if tipo == "xlsx" {
+					writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[idx], 1), col)
+				} else {
+					record = append(record, col)
+				}
+			}
+
+			if len(record) > 0 && tipo == "csv" {
+				writeDataCsvFile(newCsv, record)
 			}
 
 			continue
@@ -117,6 +197,8 @@ func generateFile(base string, status string) *excelize.File {
 		if _, ok := pedidoList[row[0]]; ok {
 			cancelado = "SIM"
 		}
+
+		var record []string
 
 		for col, value := range row {
 			letterIdx := col
@@ -130,8 +212,14 @@ func generateFile(base string, status string) *excelize.File {
 			}
 
 			if col == 4 {
-				writeCellValue(newSheet, fmt.Sprintf("%v%v", "E", index+1), cancelado)
-				writeCellValue(newSheet, fmt.Sprintf("%v%v", "F", index+1), value)
+				if tipo == "xlsx" {
+					writeCellValue(newSheet, fmt.Sprintf("%v%v", "E", index+1), cancelado)
+					writeCellValue(newSheet, fmt.Sprintf("%v%v", "F", index+1), value)
+				} else {
+					record = append(record, cancelado)
+					record = append(record, value)
+				}
+
 				continue
 			}
 
@@ -140,30 +228,65 @@ func generateFile(base string, status string) *excelize.File {
 					corte = "COM CORTE"
 				}
 
-				writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), corte)
+				if tipo == "xlsx" {
+					writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), corte)
+				} else {
+					record = append(record, corte)
+				}
+
 				continue
 			}
 
 			if col >= 16 && col <= 18 && row[15] == "lab" && row[12] == row[16] {
-				writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), "")
+				if tipo == "xlsx" {
+					writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), "")
+				} else {
+					record = append(record, "")
+				}
+
 				continue
 			}
 
 			if col >= 12 && col <= 14 && row[15] == "optica" {
-				writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), row[col+4])
+				if tipo == "xlsx" {
+					writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), row[col+4])
+				} else {
+					record = append(record, row[col+4])
+				}
+
 				continue
 			}
 
 			if col >= 16 && col <= 18 && row[15] == "optica" {
-				writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), "")
+				if tipo == "xlsx" {
+					writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), "")
+				} else {
+					record = append(record, "")
+				}
+
 				continue
 			}
 
-			writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), value)
+			if tipo == "xlsx" {
+				writeCellValue(newSheet, fmt.Sprintf("%v%v", letters[letterIdx], index+1), value)
+			} else {
+				record = append(record, value)
+			}
+		}
+
+		if len(record) > 0 && tipo == "csv" && newCsv != nil {
+			err := newCsv.Write(record)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 
-	return newSheet
+	if tipo == "xlsx" {
+		return newSheet, nil
+	}
+
+	return nil, newCsv
 }
 
 func writeFile(sheet *excelize.File, name string) error {
@@ -176,8 +299,17 @@ func writeFile(sheet *excelize.File, name string) error {
 	return nil
 }
 
+func getFileName(fileName string) string {
+	timeStamp := time.Now().Format("2006_01_02_15_04_05")
+
+	if filepath.Ext(fileName) == ".csv" {
+		return fmt.Sprintf("%v.csv", timeStamp)
+	}
+
+	return fmt.Sprintf("%v.xlsx", timeStamp)
+}
+
 func main() {
-	// cmd.Execute()
 	var arquivoBase string
 	var arquivoStatus string
 	var arquivoFinal string
@@ -226,17 +358,20 @@ func main() {
 				return nil
 			}
 
-			sheet := generateFile(arquivoBase, arquivoStatus)
-			fileName := fmt.Sprintf("%v.xlsx", time.Now().Format("2006_01_02_15_04_05"))
+			fileName := getFileName(arquivoBase)
+
+			sheet, _ := generateFile(arquivoBase, arquivoStatus, fileName)
 
 			if arquivoFinal != "" {
 				fileName = arquivoFinal
 			}
 
-			err := writeFile(sheet, fileName)
-			if err != nil {
-				fmt.Println(fileName)
-				log.Fatalln("Erro ao gerar a planilha final. Erro: ", err)
+			if sheet != nil {
+				err := writeFile(sheet, fileName)
+				if err != nil {
+					fmt.Println(fileName)
+					log.Fatalln("Erro ao gerar a planilha final. Erro: ", err)
+				}
 			}
 
 			return nil
